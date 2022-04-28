86b59f0225f8c7af1329a054bf43a2bf79b69067
wwyyww <yso00302@gmail.com>
2022-04-28 20:36:41 +0900

    [LeetCode] #70. Climbing Stairs

---
+++ LeetCode/dynamic_programming/climbing-stairs.py
@@ -0,0 +1,19 @@
+#70. Climbing Stairs
+#문제 링크 : https://leetcode.com/problems/climbing-stairs/
+
+#책 풀이
+import collections
+
+
+class Solution:
+    dp = collections.defaultdict(int)
+
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        if self.dp[n]:
+            return self.dp[n]
+        self.dp[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.dp[n]
+
