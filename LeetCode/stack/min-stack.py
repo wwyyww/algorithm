b9b8a1aaabb7bc25c63fe1e35b11b99c38b8f6ec
wwyyww <yso00302@gmail.com>
2022-05-19 20:33:42 +0900

    [LeetCode] #155. Min Stack

---
+++ LeetCode/stack/min-stack.py
@@ -0,0 +1,28 @@
+#155. Min Stack
+#문제 링크 : https://leetcode.com/problems/min-stack/
+
+#내 풀이
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        del self.stack[-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
