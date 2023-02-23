af5b346b363b7609f5770a9ef9dd64dd81bf81ff
wwyyww <yso00302@gmail.com>
2023-02-23 16:14:54 +0900

    [Programmers] #12919. 서울에서 김서방 찾기

---
+++ Programmers/string/서울에서 김서방 찾기.py
@@ -0,0 +1,10 @@
+#12919. 서울에서 김서방 찾기
+#문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+#내 풀이
+
+def solution(seoul):
+    idx=seoul.index('Kim')
+    answer='김서방은 '+str(idx)+'에 있다'
+    
+    return answer
