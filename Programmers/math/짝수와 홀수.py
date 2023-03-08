3792c4b228f1a00f8b9f7985c8859e06a6cabf60
wwyyww <yso00302@gmail.com>
2023-03-08 19:04:51 +0900

    [Programmers] #12937. 짝수와 홀수

---
+++ Programmers/math/짝수와 홀수.py
@@ -0,0 +1,11 @@
+#12937. 짝수와 홀수
+#문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+#내 풀이
+
+def solution(num):
+
+    if num%2==0:
+        return 'Even'
+    else:
+        return 'Odd'
